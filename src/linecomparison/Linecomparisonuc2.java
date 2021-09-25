cba5e8206653196ae11b13496043a76fc8d6a88e
RAHUL LABADE <rahullabade05@gmail.com>
2021-09-25 21:46:07 +0530

    Added: Linecomparisonuc2

--- src/linecomparison/Linecomparisonuc2.java
+++ src/linecomparison/Linecomparisonuc2.java
@@ -1,10 +1,18 @@
 package linecomparison;
 
 public class Linecomparisonuc2 {
-
+	double distance(int x1,int y1,int x2,int y2)
+	{
+		double distance;
+		distance=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		return distance;
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Linecomparisonuc1 obj = new Linecomparisonuc1();
+		double line1 = obj.distance(1,1 , 1, 1);
+		double line2 = obj.distance(1,2 , 3, 4);
+		System.out.println("Distance"+line1);
+		System.out.println("Distance"+line2);
 	}
 
 }
--- src/linecomparison/Linecomparisonuc3.java
+++ src/linecomparison/Linecomparisonuc3.java
@@ -1,10 +1,27 @@
 package linecomparison;
 
 public class Linecomparisonuc3 {
-
-	public static void main(String[] args) {
-		
-
+	double distance(int x1 , int y1 , int x2 , int y2)
+	{
+		double distance;
+		distance=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		return distance;
 	}
+	public static void main(String[] args) {
+		Linecomparisonuc3 obj = new Linecomparisonuc3();
+		double line1 = obj.distance(1,6,1,9);
+		double line2 = obj.distance(6,1,8,1);
+		if(Double.compare(line1,line2)==0)
+		{
+			System.out.println("line1 and line 2 are equal");
+		}
+		else if(Double.compare(line1,line2) < 0)
+		{
+			System.out.println("line1 is less than line2");
+		}
+		else
+			System.out.println("line1 is greater than line2");
+	}
+	
 
 }
